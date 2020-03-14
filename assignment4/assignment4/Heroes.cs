06f624d4129d6ec58cfb5937f0d41c650fa908c2
Nicholas Claudio <nclaudio449@gmail.com>
2020-03-14 17:00:11 -0500

    Completed Assignment 4

---
+++ assignment4/assignment4/Heroes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment4
+{
+    class Heroes
+    {
+        public IEnumerable<string> SuperHeroes()
+        {
+            yield return "Thor";
+            yield return "Hulk";
+            yield return "Luke Cage";
+            yield return "Wolverine";
+            yield return "Starfire";
+            yield return "Cyborg";
+            yield return "Raven";
+            yield return "Beast Boy";
+            yield return "Robin";
+        }
+    }
+}
---
+++ assignment4/assignment4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Heroes super = new Heroes();
+
+
+            foreach (string value in super.SuperHeroes())
+            {
+                Console.WriteLine(value);
+            }
+
+            Console.ReadKey();
+            
+        }
+    }
+}
